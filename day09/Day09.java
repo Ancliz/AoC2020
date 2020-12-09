b2e471cd4c687dc8c579d26eb043cf4882b4353d
Alasdair Moloney <s3785526@student.rmit.edu.au>
2020-12-09 23:43:29 +1100

    Day09

--- day09/Day09.java
+++ day09/Day09.java
@@ -1,6 +1,8 @@
 package day09;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import day.Day;
@@ -19,35 +21,62 @@ public class Day09 extends Day {
 
     @Override
     public void run() {
-        long[] pair = findPair();
+        findPair();
         System.out.println(input.get(sumIndex));
+        System.out.println("Weakness: " + findEncryptionWeakness());
     }
 
     private long[] findPair() {
-        boolean found = false;
         long x = 0, y = 0;
         int curr = 0;
 
+        for(int i = curr; i < sumIndex; ++i) {
+            if(inPrev25(i, input.get(sumIndex) - input.get(i))) {
+                sumIndex++;
+                curr = sumIndex - PREAMBLE_LEN;
+                i = curr - 1;
+            }
+        }
+        return new long[] { x, y };
+    }
+
+    private boolean inPrev25(int curr, long num) {
+        if (num < 0)
+            return false;
+        for (int i = curr; i < sumIndex; ++i) {
+            if (input.get(i) == num)
+                return true;
+        }
+        return false;
+    }
+
+    private long findEncryptionWeakness() {
+        List<Long> addends = new ArrayList<Long>();
+        long sought = input.get(sumIndex);
+        long sum = 0;
+        int lower = 0;
+        boolean found = false;
+
         while(!found) {
-
-            for(int i = curr + 1; i < sumIndex; ++i) {
-                x = input.get(curr);
-                y = input.get(i);
-
-                if(x + y == input.get(sumIndex)) {
-                    sumIndex++;
-                    curr = sumIndex - PREAMBLE_LEN;
+            for(int i = lower; i < sumIndex; ++i) {
+                sum += input.get(i);
+                addends.add(input.get(i));
+                
+                if(sum > sought)
                     break;
-                }
-                else if(sumIndex - 2 == curr) {
+
+                if(sum == sought) {
+                    Collections.sort(addends);
                     found = true;
                     break;
-                }            
+                }
             }
-            if(x + y != input.get(sumIndex - 1))
-                curr++;
+            if(!found) {
+                lower++;
+                sum = 0;
+                addends.clear();
+            }
         }
-        return new long[] {x, y};
+        return addends.get(0) + addends.get(addends.size() - 1);
     }
-    
 }
